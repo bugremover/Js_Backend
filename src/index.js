5c15d10a182fad38c1553daf0ced228ab6e59004
Harshavardhan K <harshavardhan75anc@gmail.com>
2024-11-02 22:43:55 +0530

    connected to database in local

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// import connectDB from "./db/index.js";
+import mongoose from "mongoose";
+import {DB_NAME} from './constants.js';
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path:'.env'
+});
+
+connectDB();
+
+
+// import express from "express"
+// const app = express()
+// ;( async() => {
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/
+//             ${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("ERRR:",error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+//     }
+//     catch(error){
+//         console.error("Error:",error)
+//         throw error
+//     }
+// })()
+
+
+//1. when were we tried to talk with database for sure will get errors
+// so use try catch while connecting to database
+//Database is always in another continent
+//have to add async await
